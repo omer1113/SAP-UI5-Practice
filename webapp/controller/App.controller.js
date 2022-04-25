d47be19410459c320834800b6bda5304bb31277f
Omer-S <omer1113@gmail.com>
2022-04-25 17:38:22 +0300

    in index.html, the meta and title tags were moved from inside to above the script tag

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,12 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
-], function(Controller){
-    "use strict"
-    return Controller.extend("sap.ui.demo.walkthrough.App",{
-        onShowHello: function() {
-            alert("Hello!");
-        }
-    })
-})
+ ], function (Controller) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+       onShowHello : function () {
+          // show a native JavaScript alert
+          alert("Hello World");
+       }
+    });
+ });
+ 
